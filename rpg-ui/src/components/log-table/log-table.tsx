31818d58b2776b0728ccc4da3fceba5e9cb4be0d
Jordan Western <ek9_fly@hotmail.co.uk>
2024-06-30 23:15:09 +0100

    dense table, resize

--- rpg-ui/src/components/log-table/log-table.tsx
+++ rpg-ui/src/components/log-table/log-table.tsx
@@ -20,29 +20,58 @@ const dummyDataRows = [
     "You travelled to Mordor. Its a vast landscape, filled with rows of Trolls and Orcs weilding crudely forged weapons. It looks like they might be preparing for war. Nothing has changed since the last time you came here."
   ),
   createData("19:39:01", "You encountered a troll."),
-  createData("19:39:11", "You gave 35 damage to a troll."),
+  createData("19:39:11", "You dealt 35 damage to a troll."),
   createData("19:39:15", "You took 22 damage from a troll."),
+  createData("19:39:30", "You dealt 35 damage to a troll."),
+  createData(
+    "19:39:31",
+    "The troll died. You looted the troll of the minor healing potion."
+  ),
+  createData(
+    "19:39:59",
+    "You travelled to the Forbiddon forest. It's cold and quiet."
+  ),
+  createData("19:40:01", "You encountered a dire wolf."),
+  createData("19:36:04", "You took 15 damage from a skeleton."),
+  createData("19:36:56", "You drank a health potion."),
+  createData(
+    "19:38:33",
+    "You travelled to Mordor. Its a vast landscape, filled with rows of Trolls and Orcs weilding crudely forged weapons. It looks like they might be preparing for war. Nothing has changed since the last time you came here."
+  ),
+  createData("19:39:01", "You encountered a troll."),
+  createData("19:39:11", "You dealt 35 damage to a troll."),
+  createData("19:39:15", "You took 22 damage from a troll."),
+  createData("19:39:30", "You dealt 35 damage to a troll."),
+  createData(
+    "19:39:31",
+    "The troll died. You looted the troll of the minor healing potion."
+  ),
+  createData(
+    "19:39:59",
+    "You travelled to the Forbiddon forest. It's cold and quiet."
+  ),
+  createData("19:40:01", "You encountered a dire wolf.")
 ];
 
 export default function LogTable() {
   return (
-    <TableContainer component={Paper} sx={{ maxHeight: 300 }}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Time</TableCell>
-            <TableCell>Log message</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {dummyDataRows.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell>{row.time}</TableCell>
-              <TableCell>{row.logMessage}</TableCell>
+      <TableContainer component={Paper} sx={{ height: 500, minHeight: 200 }}>
+        <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table" size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Time</TableCell>
+              <TableCell>Log message</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {dummyDataRows.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell>{row.time}</TableCell>
+                <TableCell>{row.logMessage}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
   );
 }
