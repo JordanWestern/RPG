111bebbfdbe435c0e2c4f3a99f93f9a7e894f39b
Jordan Western <ek9_fly@hotmail.co.uk>
2024-06-30 19:59:35 +0100

    fix warning

--- rpg-ui/src/app.tsx
+++ rpg-ui/src/app.tsx
@@ -2,22 +2,25 @@ import { createRoot } from "react-dom/client";
 import { Home } from "./pages/home";
 import React from "react";
 import { ThemeProvider } from "@emotion/react";
-import { ThemeOptions, createTheme } from "@mui/material";
+import { createTheme } from "@mui/material";
 
 // https://zenoo.github.io/mui-theme-creator/
 const theme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#ffa000',
+      main: "#ffa000",
     },
     secondary: {
-      main: '#9e9e9e',
+      main: "#9e9e9e",
     },
   },
 });
 
-const root = createRoot(document.body);
+const container = document.getElementById("app");
+
+const root = createRoot(container!);
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
