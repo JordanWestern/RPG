04e3ba06c0fcec67412216e1339b0209185ec2d1
Jordan Western <ek9_fly@hotmail.co.uk>
2024-07-17 21:23:30 +0100

    slight issue when moving file resolved

--- API/RPG.App/EventHandlers/PlayerCreatedEventHandler.cs
+++ API/RPG.App/EventHandlers/PlayerCreatedEventHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace RPG.App.EventHandlers
+﻿using MediatR;
+using RPG.App.Services;
+using RPG.Domain.Entities;
+using RPG.Domain.Events;
+using RPG.Domain.Repositories;
+
+namespace RPG.App.EventHandlers;
+
+public class PlayerCreatedEventHandler(IGameLogRepository gameLogRepository, IGameEventService gameEventService) : INotificationHandler<PlayerCreatedEvent>
 {
-    internal class PlayerCreatedEventHandler
+    public async Task Handle(PlayerCreatedEvent @event, CancellationToken cancellationToken)
     {
+        var log = GameLog.Create(@event.Player.Id, $"Player was created. Id: {@event.Player.Id} Name: {@event.Player.Name}");
+        await gameLogRepository.CreateLog(log, cancellationToken);
+        await gameEventService.EmitGameEvent(log, cancellationToken);
     }
 }
